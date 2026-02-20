acad6ab37c9ff9b0327f5a453b52a3c7e25ebbee
Mbanks-lab46 <85198700+Mbanks-lab46@users.noreply.github.com>
2026-02-20 13:30:22 -0600

    Pushing more new features

--- e-shop/src/app/products/services/products.service.ts
+++ e-shop/src/app/products/services/products.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { ProductTypes } from '../product-types';
 import { environment } from '../../../environments/environment';
 
@@ -8,12 +8,18 @@ import { environment } from '../../../environments/environment';
   providedIn: 'root',
 })
 export class ProductsService {
-  apiUrl: string = environment.apiUrl;
-  constructor(private _http: HttpClient) {
-
-  }
+  public apiUrl = environment.apiUrl;
+  private _products: ProductTypes[] = [];
+  constructor(private _http: HttpClient) {}
 
   getProduct(): Observable<ProductTypes[]> {
     return this._http.get<ProductTypes[]>(`${this.apiUrl}/products`);
+  }
+
+  addProduct(newProduct: Partial<ProductTypes>): Observable<ProductTypes> {
+    return this._http.post<ProductTypes>(`${this.apiUrl}/products`, newProduct).pipe(map(product => {
+      this._products.push(product);
+      return product;
+    }));
   }
 }
